b8a8cca99029e391d7b57abafce28f570773396d
tharindung <gtnadeesha@yahoo.com>
2026-04-10 15:00:58 -0400

    Updated main class with an annotation

--- src/main/java/com/ccws/aptbook/AptbookApplication.java
+++ src/main/java/com/ccws/aptbook/AptbookApplication.java
@@ -1,5 +1,6 @@
 package com.ccws.aptbook;
 
+import jakarta.annotation.PostConstruct;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
@@ -10,15 +11,15 @@ public class AptbookApplication {
 
 	private static final Logger logger = LoggerFactory.getLogger(AptbookApplication.class);
 
+	@PostConstruct
 	public void init()
 	{
 		logger.info("Starting application execution....");
 	}
 
 	public static void main(String[] args) {
-
-		SpringApplication.run(AptbookApplication.class, args);
 		logger.info("Application exeuted....");
+		SpringApplication.run(AptbookApplication.class, args);
 	}
 
 }
